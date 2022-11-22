b39383c761cbc0021792c39c6f0df2dbae4a3d95
aruadecarvalho <aruadecarvalho@gmail.com>
2022-11-22 16:23:18 -0300

    removed Button component, added LoginForm component Co-authored-by: harris <harrisxo@users.noreply.github.com>

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
+import LoginForm from "../LoginForm/LoginForm";
 import "./Home.css";
-import Button from "../Button/Button";
 import KiwiImg from "../../assets/imgs/kiwi1.jpg";
 
 const Home = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
   return (
     <div className="home--container">
       <div className="home-hero-img--wrapper">
@@ -11,9 +13,19 @@ const Home = () => {
       <div className="home-hero--wrapper">
         <h1>Virtual Kiwi!</h1>
         <p class="desc">Welcome to your personal kiwi!</p>
+        {showLoginForm && (
+          <>
+            <LoginForm />
+          </>
+        )}
         <div className="home-button--wrapper">
-          <Button buttonType="base" text="Login" />
-          <Button buttonType="inverted" text="Register" />
+          <button
+            className="button button-base"
+            onClick={() => setShowLoginForm(true)}
+          >
+            Login
+          </button>
+          <button className="button button-inverted">Register</button>
         </div>
       </div>
     </div>
---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import "./LoginForm.css";
+
+const LoginForm = () => {
+  return (
+    <div className="login-form--container">
+      <input className="form-input" placeholder=" Your Username" />
+      <input className="form-input" placeholder=" Your Password" />
+    </div>
+  );
+};
+
+export default LoginForm;
